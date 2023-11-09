2503e410840f55dc0b6f54561ebc5d8c863f30fb
Nathan Temiquel <ntemiquel@gmail.com>
2023-11-09 14:26:28 -0500

    updated docstrings

--- utils/test_model.py
+++ utils/test_model.py
@@ -44,6 +44,20 @@ class TestModel:
     def __init__(
         self, model, mod_sel, fout_path, X_test, y_test, id_val, threshold, fast, test
     ):
+        """
+        Args:
+            model: keras or scikit-learn model object
+            mod_sel: string name of model, used for configuration. Current
+                options: "cnn_raw"
+            fout_path: string filepath for output files
+            X_test: ndarray-like object of test set features
+            y_test: array-like object of test set labels, must be same lengh as X_test
+            id_val: string prepended to output file names
+            threshold: float decision threshold for positive identification
+            fast: bool supresses output files for quick testing if true
+            test: bool identifies output files as test files if true, validation if
+                false
+        """
         self.model = model
         self.mod_sel = mod_sel
         self.X_test = X_test
@@ -60,8 +74,16 @@ class TestModel:
         self.test_cnn_model()
 
     def run_pred(self):
-        # todo: docstring
-        """ """
+        """Produce predictions for all samples in X_test, this is both returned and
+            set to self.y_pred
+
+        Args:
+            None
+
+        Returns:
+            y_pred: array of predictions for each sample
+        
+        """
 
         # predict classes for provided test set
         self.y_pred = self.model.predict(
@@ -69,20 +91,15 @@ class TestModel:
         return self.y_pred
 
     def test_cnn_model(self):
-        # todo: docstring update
         """
         test a trained model with given parameters, creates a csv of confusion matrix
         at Model Data/CNN Model/ 'id_val'+comfmatout.csv
 
         Args:
-          model: a trained keras model used to predict the categories of the test set
-          X_test: a DataFrame or ndarray of sample features sets for testing
-          y_test: a DataFrame or Series of sample labels the X_test feature set - must
-            be in the same order as the X_test feature set
-          id_val: a string used in the file name of the outputs for identification
+          None
 
         Returns:
-          None; creates a file at the /Model Data/CNN Model/Raw/ folder
+          None. Creates a file at the /Model Data/CNN Model/Raw/ folder
         """
 
         # report confusion matrix
@@ -96,26 +113,24 @@ class TestModel:
                 file_n = self.id_val + "_test"
             else:
                 file_n = self.id_val + "_validation"
-            # save confusion matrix as csv to drive
+            # save confusion matrix as csv to directory
             confmatout_path = self.fout_path + file_n
-
             confmat.to_csv(confmatout_path + r"_confmat.csv")
+
             # save output weights
             pd.DataFrame(
                 data=self.model.predict(self.X_test), index=self.y_test.index.values
             ).to_csv(confmatout_path + "_probs.csv")
 
     def dec_pred(self):
-        # todo: docstring update
         """takes prediction weights and applies a decision threshold to deterime the
         predicted class for each sample
 
         Args:
-          y_pred: an ndarray of prediction weights for a set of samples
-          threshold: the determination threshold at which the model makes a prediction
+          None
 
         Returns:
-          a 1-d array of class predictions, unknown classes are returned as class 6
+          a 1-d array of class predictions, unknown classes are returned as class 15
         """
 
         probs_ls = np.amax(self.y_pred, axis=1)
@@ -129,17 +144,14 @@ class TestModel:
         return pred_lab
 
     def build_confmat(self):
-        # todo: docstring update
         """builds the confusion matrix with labeled axes
 
         Args:
-          y_label: a list of true labels for each sample
-          y_pred: a list of predicted labels for each samples
-          threshhold: the decision threshhold for the mat_labels
+            None
 
         Returns:
-          A DataFrame containing the confusion matrix, the column names are the
-          predicted labels while the row indices are the true labels
+            DataFrame containing the confusion matrix, the column names are the
+            predicted labels while the row indices are the true labels
         """
         print("y_pred=", self.y_pred)
         print("y_dec_pred=", self.y_dec_pred, "\n\n\ny_label=", self.y_test)
@@ -154,7 +166,6 @@ class TestModel:
         )
 
     def plot_roc(self, X_df, i):
-        # TODO: docstring update
         """plots the receiver operating characteristic curve for the data in X_df with
         true binary labels in the final column
 
@@ -190,7 +201,6 @@ class TestModel:
         return tpr_p, fpr_p, thresh
 
     def roc_all(self, outputs, labels):
-        # todo: docstring update
         """creates ROC curves for each class in the output of a classifier
 
         Args:
